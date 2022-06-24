28b48c4a4cf145fb2bf22e890b8f8ce0dfab1068
honaseung <lhs5948@gmail.com>
2022-06-25 07:27:53 +0900

    create new branch

--- 섹션 5/2. 쇠막대기/AA.py
+++ 섹션 5/2. 쇠막대기/AA.py
@@ -2,5 +2,19 @@ import sys
 sys.stdin = open('input.txt')
 
 line = input()
+pipesCnt = 0
+piecesCnt = 0
+stack = []
 
-print(line)
+for presentOrder in line :
+    if presentOrder == '(' :
+        pipesCnt += 1
+    else :
+        lastOrder = stack[-1]
+        if lastOrder == '(' :
+            pipesCnt -= 1
+            piecesCnt += pipesCnt
+        else :
+            pipesCnt -= 1
+    stack.append(presentOrder)
+print(piecesCnt)
