fee658712defa604a8c393a4131609831f057f9e
honaseung <lhs5948@gmail.com>
2022-06-27 13:12:33 +0900

    the answer of the quiz

--- 섹션 5/2. 쇠막대기/AA.py
+++ 섹션 5/2. 쇠막대기/AA.py
@@ -4,17 +4,33 @@ sys.stdin = open('input.txt')
 line = input()
 pipesCnt = 0
 piecesCnt = 0
-stack = []
+stack = [line[0]]
 
-for presentOrder in line :
-    if presentOrder == '(' :
-        pipesCnt += 1
-    else :
-        lastOrder = stack[-1]
-        if lastOrder == '(' :
-            pipesCnt -= 1
-            piecesCnt += pipesCnt
+for i in range(1, len(line)) :
+    presentOrder =  line[i]
+    lastOrder = stack[-1]
+    if lastOrder == '(' :
+        if presentOrder == '(' :
+            pipesCnt += 1
         else :
+            piecesCnt += pipesCnt
+    else : 
+        if presentOrder == ')' :
             pipesCnt -= 1
+            piecesCnt += 1
+
     stack.append(presentOrder)
 print(piecesCnt)
+'''
+stack = []
+for x in line :
+    if x == '(' :
+        stack.append(x)
+    else :
+        stack.pop()
+        if stack[-1] == '(' :
+            piecesCnt += len(stack)
+        else :
+            piecesCnt += 1
+print(piecesCnt)
+'''
