05ba0ecb117551ead088d2f4e9e360b8105054cc
honaseung <lhs5948@gmail.com>
2023-01-19 17:59:51 +0900

    add Q6 answer

--- 섹션 6/6. 중복순열 구하기/AA.py
+++ 섹션 6/6. 중복순열 구하기/AA.py
@@ -1,10 +1,10 @@
 import sys
-sys.stdin = open('6. 중복순열 구하기/in2.txt')
+# sys.stdin = open('input.txt')
 
 N, M = map(int, input().split())
 
 
-def printallduplicatedpermutation(permutation:list, num:int, lev: int) :
+def printallduplicatedpermutation(permutation:list, lev: int) :
     global cnt
     if (lev == M) :
         for n in permutation :
@@ -13,12 +13,10 @@ def printallduplicatedpermutation(permutation:list, num:int, lev: int) :
         print()
     else :
         for n in range(1, N + 1) :
-            permutation.append(n)
-            for m in range(1, N + 1) :
-                permutation.append(m)
-                printallduplicatedpermutation(permutation.copy(), m, lev + 1)
+            permutation[lev] = n
+            printallduplicatedpermutation(permutation, lev + 1)
 
 if __name__ == '__main__' :
     cnt = 0
-    printallduplicatedpermutation([], 1, 1)
+    printallduplicatedpermutation([0] * M, 0)
     print(cnt)
